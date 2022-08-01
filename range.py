6badc6c0b4ce14d8ed3cf17e154ed55679b7dff4
Messi Moraes <messias_gangrel@hotmail.com>
2022-08-01 09:11:22 -0300

    Add range file

---
+++ range.py
@@ -0,0 +1,22 @@
+"""
+Ranges
+
+Range são utilizados para gerar sequências numéricas, não de forma aleatória, sim de maneira especificas.
+"""
+
+#  01 - range(valor_de_parada)
+# for num in range(11):
+#     print(f" -> {num}")
+
+#  02 - range(valor_de_início, valor_de_parada)
+# for num in range(1, 10):
+#     print(num)
+
+# 03 - range(valor_de_início, valor_de_parada, passo)
+# for num in range(0, 55, 5):
+#     print(num)
+
+# 04 - range(valor_de_início, valor_de_parada, passo)
+for num in range(10, -1, -1):
+    print(num)
+
