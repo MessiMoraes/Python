83a7d71edae7608d341134bb280f995ce5528bc6
Messi Moraes <messias_gangrel@hotmail.com>
2022-08-02 07:02:04 -0300

    Add loop_while file

---
+++ loop_while.py
@@ -0,0 +1,19 @@
+"""
+Loop While
+
+O bloco será executado enquanto a expressão booleana for verdadeira.
+
+Expressão booleana é toda expressão onde o resultado é verdadeiro ou falso
+"""
+#  01
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+#  02
+resposta = ''
+while resposta != 'sim':
+    resposta = input("Já acabou Jéssica?: ")
+
+
