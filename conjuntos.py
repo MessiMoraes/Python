a6c235e2f3ba113396ff26e249ae34988ad7e354
Messi <messias_gangrel@hotmail.com>
2022-11-01 08:23:37 -0300

    Create conjuntos.py

---
+++ conjuntos.py
@@ -0,0 +1,13 @@
+"""
+Conjuntos - Em qualquer linguagem de programação, estamos fazendo referência a teoria dos conjuntos da matemática.
+- No Python os conjuntos são chamados de Sets.
+- Na mesma forma que na matemática, conjuntos não possuem:
+    - (Sets) Valores duplicados.
+    - (Sets) Valores ordenados.
+    - Não são indexados
+    - Não são acessados via índice
+
+- Usamos quando precisamos armazenas elementos, sem se importar com a ordenação, chaves, valores e itens duplicados.
+- (Sets) são referenciados em Python com chaves {}.
+
+"""
