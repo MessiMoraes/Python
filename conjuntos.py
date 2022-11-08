429e73c2aa0fc6591b33c40b8ffddef6d9baaf64
Messi <messias_gangrel@hotmail.com>
2022-11-08 08:38:17 -0300

    Update conjuntos.py

--- conjuntos.py
+++ conjuntos.py
@@ -68,11 +68,85 @@ s = set({1, 2, 3, 4, 5, 5, 6, 7, 2, 3})
 # Nós adicionamos cada cidade em uma lista em Python, já que em uma lista podemos adicionar novos elementos
 # e ter repetição.
 
-cidades = ['Belo Horizonte', 'São Paulo', 'Campo Grande', 'Cuiaba', 'Campo Grande', 'São Paulo', 'Cuiaba']
-print(cidades)
-print(len(cidades))
+# cidades = ['Belo Horizonte', 'São Paulo', 'Campo Grande', 'Cuiaba', 'Campo Grande', 'São Paulo', 'Cuiaba']
+# print(cidades)
+# print(len(cidades))
 
 # Agoras precisamos saber quantas cidades distintas, ou seja, únicas, temos.
-print(len(set(cidades)))
+# print(len(set(cidades)))
 
+# Adicionando elementos ao conjunto.
+# s = {1, 2, 3}
+# print(s)
 
+# s.add(4)
+
+# Forma 01
+# s.remove(3)  # Informamos o valor a ser removido. Não há chave.
+# print(s)
+
+# Forma 02
+# s.discard(2)  # Se o valor não for encontrado, nenhum erro é gerado.
+# print(s)
+
+# Copiando um conjunto para outro
+# Forma 01 - Deep Copy
+# novo = s.copy()
+# print(novo)
+#
+# novo.add(4)
+# print(novo)
+# print(s)
+
+# Forma 02 - Shallow Copy
+# novo = s
+# novo.add(4)
+# print(novo)
+# print(s)
+
+# Podemos remover todos os itens de um conjunto.
+# s.clear()
+# print(s)
+
+# Métodos matemáticos de conjuntos
+# Imagina que temos dois conjuntos: Um contendo os estudantes do curso Python e um contendo os estudantes
+# do curso de Java
+
+python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Julia', 'Guilherme'}
+java = {'Fernando', 'Gustavo', 'Julia', 'Ana', 'Patricia'}
+
+# Veja que alguns alunos que estudam Python também estudam Java.
+# Precisamos gerar um conjunto com nomes de estudantes únicos.
+
+# Forma 01
+# unicosOne = python.union(java)
+# print(unicosOne)
+
+# Forma 02 - Utilizando o caractere pipe |
+# unicosTwo = python | java
+# print(unicosTwo)
+
+# Gerar um conjunto de estudantes que estão em ambos os cursos.
+
+# Forma 01 - Utilizando intersection
+# ambos = java.intersection(python)
+# print(ambos)
+
+# Forma 02 - Utilizando o &
+# ambos  = java & python
+# print(ambos)
+
+# Gerar um conjunto de estudantes de um curso que não estão no outro.
+# so_python = python.difference(java)
+# print(so_python)
+#
+# so_java = java.difference(python)
+# print(so_java)
+
+# Soma*, Valor Máximo*, Valor Mínimo*, Tamanho
+s = {1, 2, 3, 4, 5, 6}
+
+print(sum(s))
+print(max(s))
+print(min(s))
+print(len(s))
