acb4705a7f054de0507d9c38ce14cb3785f5c950
Messi <messias_gangrel@hotmail.com>
2022-11-17 08:43:03 -0300

    Create definindo_funcoes.py

---
+++ definindo_funcoes.py
@@ -0,0 +1,31 @@
+"""
+Definindo Funções
+- Funções são pequenos trechos de código que realizam tarefas especificas.
+- Podem ou não receber entradas de dados e retornar uma saída de dados.
+- Muito úteis para executar procedimentos similares por repetidas vezes.
+
+Obs: Se você escrever uma função que realiza varias tarefas dentro dela é bom fazer uma verificação
+para que a função seja simplificada.
+
+"""
+
+# Exemplo de utilização de funções:
+# cores = ['Verde', 'Amarelo', 'Azul', 'Branco']
+# print(cores)  # Utilizando funções integradas (Built-in) do Python print()
+# cores.append('Roxo')
+# print(cores)
+#
+# cores.clear()
+# print(cores)
+
+# Como definir funções:
+def soma():
+    print(5 + 5)
+
+# Utilizando funções
+r = soma
+r()
+
+
+
+
